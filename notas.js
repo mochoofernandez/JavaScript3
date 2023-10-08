12545349bf7e7e7526c2512ac1268cad672f0296
mochoofernandez <143915544+mochoofernandez@users.noreply.github.com>
2023-10-08 16:52:06 -0300

    Add files via upload

---
+++ notas.js
@@ -0,0 +1,7 @@
+var primtrim = parseFloat(prompt("Ingrese la nota del 1 trimestre"));
+var seguntrim = parseFloat(prompt("Ingrese la nota del 2 trimestre"));
+var terctrim = parseFloat(prompt("Ingrese la nota del 3 trimestre"));
+
+var promedio = (primtrim + seguntrim + terctrim) / 3;
+
+alert("El promedio es: " +promedio);
